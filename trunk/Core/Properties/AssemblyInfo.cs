f5e8d8008ce1cd1a208c74ff8e362212748429aa
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2009-06-30 01:02:06 +0000

    1 - Moved the existing Eclipse version to a sub-folder since it will be a sub-product of TODD.
    2 - Created a skeleton for the SoftwareNinjas.TestOriented.Core assembly project, which will be written in C# and contain all the re-usable code for the various sub-products.
    
    3 - Added NAnt 0.85 without documentation and with a slight modification to include support for .NET 3.5
    
    4 - Added NAntContrib 0.85 without documentation and with a schema generated by the <nantschema> task.
    
    5 - Added SoftwareNinjas.Core and SoftwareNinjas.NAnt version 1.0.8, to provide re-usable code and handy NAnt tasks.

---
+++ trunk/Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ( "SoftwareNinjas.TestOriented.Core" )]
+[assembly: AssemblyDescription ( "Common code for the Test-Oriented plug-ins" )]
+[assembly: AssemblyConfiguration ( "" )]
+[assembly: AssemblyCompany ( "Software Ninjas" )]
+[assembly: AssemblyProduct ( "Test-Oriented Development & Debugging (TODD)" )]
+[assembly: AssemblyCopyright ( "(C) Copyright 2009 Software Ninjas" )]
+[assembly: AssemblyTrademark ( "" )]
+[assembly: AssemblyCulture ( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible ( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("57857ee8-1f41-4552-89a0-fa2e98ade7fc")]
