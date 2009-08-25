fcf535bc1a9b8a935b584f9d4eddeb75cd6c942a
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2009-08-25 01:17:15 +0000

    1 - Updated CORE dependencies to version 1.0.14
    2 - Introducing the UnitTest class with a template ported from the Eclipse version of TODD.

---
+++ trunk/Core/Test/UnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+using Parent = SoftwareNinjas.TestOriented.Core;
+
+namespace SoftwareNinjas.TestOriented.Core.Test
+{
+    /// <summary>
+    /// A class to test <see cref="Parent.UnitTest"/>.
+    /// </summary>
+    [TestFixture]
+    public class UnitTest
+    {
+        /// <summary>
+        /// Tests the <see cref="Parent.UnitTest.GenerateJavaMethod(String, String)"/> method with a typical use-case.
+        /// </summary>
+        [Test]
+        public void GenerateJavaMethod_Typical()
+        {
+            var expectedMethod = @"
+/**
+ * Tests the <i>unformat</i> method with
+ * TODO: write about scenario
+ */
+@Test public void unformat_TODO ( ) {
+    Unformatter unformatter = new Unformatter (  );
+	fail ( ""TODO: initialize variable(s) and expected value"" );
+	String format = ""TODO"";
+	String formatted = ""TODO"";
+	String[] actual = unformatter.unformat ( format, formatted );
+	String[] expected = new String[] { ""TODO"" };
+	assertEquals ( expected, actual );
+}
+";
+            var body = @"    Unformatter unformatter = new Unformatter (  );
+	fail ( ""TODO: initialize variable(s) and expected value"" );
+	String format = ""TODO"";
+	String formatted = ""TODO"";
+	String[] actual = unformatter.unformat ( format, formatted );
+	String[] expected = new String[] { ""TODO"" };
+	assertEquals ( expected, actual );";
+            Assert.AreEqual(expectedMethod, Parent.UnitTest.GenerateJavaMethod("unformat", body));
+        }
+    }
+}
---
+++ trunk/Core/UnitTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SoftwareNinjas.Core;
+
+namespace SoftwareNinjas.TestOriented.Core
+{
+    /// <summary>
+    /// Methods related to unit tests.
+    /// </summary>
+    public class UnitTest
+    {
+        internal const string JavaMethodBody = @"
+/**
+ * Tests the <i>{0}</i> method with
+ * TODO: write about scenario
+ */
+@Test public void {0}_TODO ( ) {{
+{1}
+}}
+";
+        /// <summary>
+        /// Generates a string representation of a Java unit test method.
+        /// </summary>
+        /// 
+        /// <param name="methodName">
+        /// The name of the method.
+        /// </param>
+        /// 
+        /// <param name="body">
+        /// The body of the method.
+        /// </param>
+        /// 
+        /// <returns>
+        /// A JUnit test method stub.
+        /// </returns>
+        public static string GenerateJavaMethod(string methodName, string body)
+        {
+            // TODO: the previous version handled a custom newLine
+            return JavaMethodBody.FormatInvariant(methodName, body);
+        }
+    }
+}
