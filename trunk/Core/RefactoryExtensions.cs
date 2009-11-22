2e03ea6ce66c6d87f59efba430c1d2520695652d
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2009-11-22 22:56:03 +0000

    1 - Updated CORE to version 1.0.32
    2 - Implemented some code documentation comment-related extension methods, in preparation for some more extensive support.

--- trunk/Core/RefactoryExtensions.cs
+++ trunk/Core/RefactoryExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+
 using ICSharpCode.NRefactory.Ast;
+using ICSharpCode.NRefactory;
+using SoftwareNinjas.Core;
 
 namespace SoftwareNinjas.TestOriented.Core
 {
@@ -73,5 +77,110 @@ namespace SoftwareNinjas.TestOriented.Core
             }
             return result;
         }
+
+        /// <summary>
+        /// Retrieves the code documentation comments associated with this <see cref="INode"/>.
+        /// </summary>
+        /// 
+        /// <param name="node">
+        /// The <see cref="INode"/> which may contain code documentation comments.
+        /// </param>
+        /// 
+        /// <returns>
+        /// A <see cref="String"/> representing the code documentation comment XML, without a comment prefix.
+        /// </returns>
+        public static string GetDocumentation(this INode node)
+        {
+            return (string) node.UserData;
+        }
+
+        /// <summary>
+        /// Sets the code documentation comments associated with this <see cref="INode"/>.
+        /// </summary>
+        /// 
+        /// <param name="node">
+        /// The <see cref="INode"/> to associate code documentation comments to.
+        /// </param>
+        /// 
+        /// <param name="special">
+        /// An <see cref="ISpecial"/> implementation which is likely an instance of <see cref="Comment"/> from which
+        /// the value of <see cref="Comment.CommentText"/> will be extracted.
+        /// </param>
+        public static void SetDocumentation (this INode node, ISpecial special)
+        {
+            if (special is Comment)
+            {
+                var comment = (Comment)special;
+                SetDocumentation (node, comment.CommentText);
+            }
+        }
+
+        /// <summary>
+        /// Sets the code documentation comments associated with this <see cref="INode"/>.
+        /// </summary>
+        /// 
+        /// <param name="node">
+        /// The <see cref="INode"/> to associate code documentation comments to.
+        /// </param>
+        /// 
+        /// <param name="documentation">
+        /// A <see cref="String"/> representing the code documentation comment XML to associate.
+        /// </param>
+        public static void SetDocumentation (this INode node, string documentation)
+        {
+            node.UserData = documentation;
+        }
+
+        internal static IList<ISpecial> Collapse (this IEnumerable<ISpecial> specials)
+        {
+            var result = new List<ISpecial> ();
+            var sb = new StringBuilder ();
+            int lastCommentStartLine;
+            int nextContiguousLine;
+            var comments = specials.Filter (IsDocumentationComment);
+            var e = comments.GetEnumerator();
+            if (e.MoveNext())
+            {
+                Comment comment = (Comment) e.Current;
+                sb.Append (comment.CommentText);
+                lastCommentStartLine = comment.StartPosition.Line;
+                nextContiguousLine = comment.EndPosition.Line;
+                while (e.MoveNext())
+                {
+                    comment = (Comment) e.Current;
+                    if(comment.StartPosition.Line != nextContiguousLine)
+                    {
+                        CreateAndAddComment (result, sb, lastCommentStartLine, comment.EndPosition.Line);
+                        sb.Length = 0;
+                        lastCommentStartLine = comment.StartPosition.Line;
+                    }
+                    sb.AppendLine();
+                    sb.Append (comment.CommentText);
+                    nextContiguousLine = comment.EndPosition.Line;
+                }
+                CreateAndAddComment (result, sb, lastCommentStartLine, nextContiguousLine - 1);
+            }
+
+            return result;
+        }
+
+        private static void CreateAndAddComment(IList<ISpecial> dest, StringBuilder sb, int startLine, int endLine)
+        {
+            var startLocation = new Location (0, startLine);
+            var endLocation = new Location (0, endLine);
+            var comment = new Comment (CommentType.Documentation, sb.ToString(), true, startLocation, endLocation);
+            dest.Add (comment);
+        }
+
+        internal static bool IsDocumentationComment (ISpecial special)
+        {
+            if (special is Comment)
+            {
+                var comment = (Comment) special;
+                return comment.CommentType == CommentType.Documentation;
+            }
+            return false;
+        }
+
     }
 }
--- trunk/Core/Test/RefactoryExtensions.cs
+++ trunk/Core/Test/RefactoryExtensions.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using ICSharpCode.NRefactory;
 using ICSharpCode.NRefactory.Ast;
 using NUnit.Framework;
+using SoftwareNinjas.Core;
 
 using Parent = SoftwareNinjas.TestOriented.Core;
 
@@ -12,6 +17,27 @@ namespace SoftwareNinjas.TestOriented.Core.Test
     [TestFixture]
     public class RefactoryExtensions
     {
+        private const string SimpleCodeDocumentationComment = @" <summary>
+ Tests the <c>Unformat</c> method with
+ TODO: write about scenario
+ </summary>";
+
+        private const string DocumentedMethod = @"
+/// <summary>
+/// Tests the <c>Unformat</c> method with
+/// TODO: write about scenario
+/// </summary>
+[Test]
+public void Unformat_TODO ( ) {
+	Assert.Fail ( ""TODO: initialize variable(s) and expected value"" );
+	string format = ""TODO"";
+	string formatted = ""TODO"";
+	string[] actual = Unformatter.Unformat ( format, formatted );
+	string[] expected = default(string[]);
+	Assert.AreEqual ( expected, actual );
+}
+";
+
 
         /// <summary>
         /// Tests the <see cref="Parent.RefactoryExtensions.GetTypeReference(ParametrizedNode,TypeDeclaration)" />
@@ -40,5 +66,81 @@ namespace SoftwareNinjas.TestOriented.Core.Test
             Assert.AreEqual(cut.Name, actual.Type);
         }
 
+        /// <summary>
+        /// Tests the <see cref="Parent.RefactoryExtensions.GetDocumentation(INode)" /> method
+        /// and the <see cref="Core.RefactoryExtensions.SetDocumentation(INode, string)"/> with
+        /// strings.
+        /// </summary>
+        [Test]
+        public void NodeDocumentation_String()
+        {
+            INode node = AbstractMethodTemplate.CreateClassUnderTest();
+            Parent.RefactoryExtensions.SetDocumentation(node, SimpleCodeDocumentationComment);
+            var actual = Parent.RefactoryExtensions.GetDocumentation(node);
+            Assert.AreEqual(SimpleCodeDocumentationComment, actual);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Parent.RefactoryExtensions.GetDocumentation(INode)" /> method
+        /// and the <see cref="Core.RefactoryExtensions.SetDocumentation(INode, ISpecial)"/> with
+        /// <see cref="ISpecial"/>.
+        /// </summary>
+        [Test]
+        public void NodeDocumentation_ISpecial_Artificial()
+        {
+            INode node = AbstractMethodTemplate.CreateClassUnderTest();
+            var comment = new Comment(CommentType.Documentation, 
+                SimpleCodeDocumentationComment, true, Location.Empty, Location.Empty);
+            Parent.RefactoryExtensions.SetDocumentation(node, comment);
+            var actual = Parent.RefactoryExtensions.GetDocumentation(node);
+            Assert.AreEqual(SimpleCodeDocumentationComment, actual);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Parent.RefactoryExtensions.GetDocumentation(INode)" /> method
+        /// and the <see cref="Core.RefactoryExtensions.SetDocumentation(INode, ISpecial)"/> with
+        /// an <see cref="ISpecial"/> instance obtained by parsing source code.
+        /// </summary>
+        [Test]
+        public void NodeDocumentation_ISpecial_ParsedFromSourceCode ()
+        {
+            var pair = ParseTypeMembers (DocumentedMethod);
+            var specials = pair.First.Collapse();
+            var member = pair.Second[0];
+            Parent.RefactoryExtensions.SetDocumentation (member, specials[0]);
+            var actual = Parent.RefactoryExtensions.GetDocumentation (member);
+            Assert.AreEqual (SimpleCodeDocumentationComment, actual);
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Parent.RefactoryExtensions.Collapse(IEnumerable&lt;ISpecial&gt;)"/> method
+        /// with a list of <see cref="ISpecial"/> instances created by parsing source code.
+        /// </summary>
+        [Test]
+        public void Collapse_ParsedFromSourceCode ()
+        {
+            var pair = ParseTypeMembers (DocumentedMethod);
+            var specials = pair.First;
+            var collapsedSpecials = Parent.RefactoryExtensions.Collapse ( specials );
+            Assert.AreEqual (1, collapsedSpecials.Count);
+            Comment comment = (Comment)collapsedSpecials[0];
+            Assert.AreEqual (2, comment.StartPosition.Line);
+            Assert.AreEqual (5, comment.EndPosition.Line);
+            Assert.AreEqual (SimpleCodeDocumentationComment, comment.CommentText);
+        }
+
+        private static Pair<IList<ISpecial>, IList<INode>> ParseTypeMembers (string code)
+        {
+            using (TextReader tr = new StringReader (code))
+            {
+                using (IParser parser = ParserFactory.CreateParser (SupportedLanguage.CSharp, tr))
+                {
+                    List<INode> members = parser.ParseTypeMembers ();
+                    Assert.AreEqual ("", parser.Errors.ErrorOutput);
+                    IList<ISpecial> specials = parser.Lexer.SpecialTracker.CurrentSpecials;
+                    return new Pair<IList<ISpecial>, IList<INode>> (specials, members);
+                }
+            }
+        }
     }
 }
