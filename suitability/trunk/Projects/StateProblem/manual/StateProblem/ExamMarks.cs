450e0f38a5d1bc36f718c7eb2ff5ca79e165c1e0
olivier.dagenais@gmail.com <olivier.dagenais@gmail.com@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-03-28 18:50:16 +0000

    Creating the "manual" variation of the StateProblem project as a copy of the "base" variation.

---
+++ suitability/trunk/Projects/StateProblem/manual/StateProblem/ExamMarks.cs
@@ -0,0 +1,28 @@
+﻿namespace StateProblem
+{
+    public class ExamMarks
+    {
+        // ReSharper disable InconsistentNaming
+        private const int MAX_MARK = 100;
+        private static int num_top = 0;
+        private static int last_year_top = 1;
+
+        public void has_max(int mark)
+        {
+            if (mark == MAX_MARK)
+                num_top++;
+        }
+
+        public int get_num_top()
+        { return num_top; }
+
+        public void compare()
+        {
+            if(get_num_top() > last_year_top)
+            {
+                /* target branch */
+            }
+        }
+        // ReSharper restore InconsistentNaming
+    }
+}
