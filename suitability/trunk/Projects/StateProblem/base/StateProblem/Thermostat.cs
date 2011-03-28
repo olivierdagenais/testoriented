315917cf79a6c8a152f8301639cc29911d6a8972
olivier.dagenais <olivier.dagenais@gmail.com>
2011-03-28 16:34:38 +0000

    Implemented a C# version of the thermostat example from Figure 1.

--- suitability/trunk/Projects/StateProblem/base/StateProblem/Thermostat.cs
+++ suitability/trunk/Projects/StateProblem/base/StateProblem/Thermostat.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace StateProblem
+﻿namespace StateProblem
 {
     public class Thermostat
     {
+        // ReSharper disable InconsistentNaming
+        private const int TEMP_THRESHOLD = 18;
+        private const int POWER_THRESHOLD = 50;
+
+        public static int thermostat(int temp, int power)
+        {
+            int heater_on;
+
+            if (temp < TEMP_THRESHOLD)
+                heater_on = 1;
+            else
+                heater_on = 0;
+
+            if (heater_on == 1)
+                if (power < POWER_THRESHOLD)
+                    heater_on = 0;
+            return heater_on;
+        }
+        // ReSharper restore InconsistentNaming
     }
 }
