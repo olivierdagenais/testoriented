5960ea5742bd5c738cb0d012e1d7ab98b572c53c
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-03-31 20:54:17 +0000

    Adding two unit tests covering very basic Textile functionality

---
+++ suitability/trunk/Projects/Textile/eric/Textile.Tests/Tests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace Textile.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Hyperlink()
+        {
+            const string input = @"""get lost"":http://google.ca";
+            const string expected = "<p><a href=\"http://google.ca\">get lost</a></p>\r\n";
+            var actual = TextileFormatter.FormatString(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void BulletList()
+        {
+            const string input = @"* me
+* is
+* the
+* hello";
+            const string expected = @"<ul>
+<li>me</li>
+<li>is</li>
+<li>the</li>
+<li>hello</li>
+</ul>
+";
+            var actual = TextileFormatter.FormatString(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
