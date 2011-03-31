75ba6950da52983ae65aae1b65c1e53e93c3e79c
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-03-31 21:03:05 +0000

    Merging the StateFactory class into the TextileFormatter class

--- suitability/trunk/Projects/Textile/eric/Textile/TextileFormatterStates.cs
+++ suitability/trunk/Projects/Textile/eric/Textile/TextileFormatterStates.cs
@@ -14,6 +14,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using Textile.States;
+
 #endregion
 
 
@@ -99,9 +101,8 @@ namespace Textile
         private string HandleFormattingState(string input)
         {
             {
-                var factory = new StateFactory(this);
                 Match match;
-                var formatterState = factory.Find(input, out match);
+                var formatterState = FindState(input, out match);
                 if (formatterState != null)
                 {
                     var result = formatterState.Consume(input, match);
@@ -126,6 +127,82 @@ namespace Textile
                 ChangeState(new States.ParagraphFormatterState(this));
             }
             return input;
+        }
+
+        #endregion
+
+        #region State Registration
+
+        private static readonly Dictionary<Regex,Func<TextileFormatter,FormatterState>> stateRegex = new Dictionary<Regex, Func<TextileFormatter,FormatterState>>();
+
+        private static void RegisterAllStates()
+        {
+            Register(f => new HeaderFormatterState(f),
+                SimpleBlockFormatterState.PatternBegin + @"h[0-9]+" + SimpleBlockFormatterState.PatternEnd);
+
+            Register(f => new BlockQuoteFormatterState(f),
+                SimpleBlockFormatterState.PatternBegin + @"bq" + SimpleBlockFormatterState.PatternEnd);
+
+            Register(f => new ParagraphFormatterState(f),
+                SimpleBlockFormatterState.PatternBegin + @"p" + SimpleBlockFormatterState.PatternEnd);
+
+            Register(f => new FootNoteFormatterState(f),
+                SimpleBlockFormatterState.PatternBegin + @"fn[0-9]+" + SimpleBlockFormatterState.PatternEnd);
+
+            Register(f => new OrderedListFormatterState(f),
+                ListFormatterState.PatternBegin + @"#+" + ListFormatterState.PatternEnd);
+
+            Register(f => new UnorderedListFormatterState(f),
+                ListFormatterState.PatternBegin + @"\*+" + ListFormatterState.PatternEnd);
+
+            Register(f => new TableFormatterState(f),
+                @"^\s*(?<tag>table)" +
+                   Globals.SpanPattern +
+                   Globals.AlignPattern +
+                   Globals.BlockModifiersPattern +
+                   @"\.\s*$");
+
+            Register(f => new TableRowFormatterState(f),
+                @"^\s*(" + Globals.AlignPattern + Globals.BlockModifiersPattern + @"\.\s?)?" 
+                    + @"\|(?<content>.*)\|\s*$");
+
+            Register(f => new CodeFormatterState(f),
+                @"^\s*<code" + Globals.HtmlAttributesPattern + ">");
+
+            Register(f => new PreCodeFormatterState(f),
+                SimpleBlockFormatterState.PatternBegin + @"bc" + SimpleBlockFormatterState.PatternEnd);
+
+            Register(f => new PreFormatterState(f),
+                @"^\s*<pre" + Globals.HtmlAttributesPattern + ">");
+
+            Register(f => new NoTextileFormatterState(f),
+                @"^\s*((?<tag><notextile>)\s*$|(?<tag>(notextile.))\s*)");
+
+            //Register(f => new PassthroughFormatterState(f),
+            //    @"^\s*<(h[0-9]|p|pre|blockquote)" + Globals.HtmlAttributesPattern + ">");
+        }
+
+        private static void Register(Func<TextileFormatter, FormatterState> constructor, string pattern)
+        {
+            var r = new Regex(pattern, RegexOptions.Compiled);
+            stateRegex.Add(r, constructor);
+        }
+
+        // TODO: Do without "out" parameters
+        internal FormatterState FindState(string input, out Match match)
+        {
+            foreach(var regex in stateRegex.Keys)
+            {
+                match = regex.Match(input);
+                if (match.Success)
+                {
+                    var result = stateRegex[regex](this);
+                    return result;
+                }
+            }
+
+            match = null;
+            return null;
         }
 
         #endregion
