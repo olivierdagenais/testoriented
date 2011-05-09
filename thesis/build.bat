e743e48929f442ab3a09a760a749eb68bb1f3d06
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-05-09 01:34:07 +0000

    Replaced most of the batch file with an MSBuild script that will fail faster and allow more complicated assembly.

--- thesis/build.bat
+++ thesis/build.bat
@@ -1,21 +1,5 @@
 @echo off
-rmdir /s /q bin
-mkdir bin
-copy dalthesis.cls bin\
-copy thesis.bib bin\
-xsl thesis.itex iTeX.xsl bin\thesis.tex
-cd bin
-
-rem http://www.latex-community.org/forum/viewtopic.php?f=5&t=2011&start=0
-rem 1. Run LaTeX to generate the AUX file. It adds a line every time it finds a cite.
-latex thesis.tex
-rem 2. Then run BibTeX (executable: bibtex) to generate the BBL (from the AUX lines).
-bibtex thesis.aux
-rem 3. Then run LaTeX again, which will include the BBL.
-latex thesis.tex
-rem 4. Then run LaTeX again to make any updates corresponding to including the possibly massive BBL
-rem (e.g., updating your final page count on each of your pages, updating your ToC, etc.).
-latex thesis.tex
-rem Generating DVI then converting to PDF yields smaller files than the one-step pdflatex
-dvipdfm -p letter thesis.dvi
-cd ..
+setlocal
+call "%VS100COMNTOOLS%\vsvars32.bat"
+msbuild.exe thesis.msbuild /nologo /verbosity:minimal %*
+endlocal
