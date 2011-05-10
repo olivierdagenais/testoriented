923fb61da3d1f3b94982afa73388b9666eae8243
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-05-10 00:57:35 +0000

    Added the Stateful project with SmokeDetector and SmokeDetectorTest samples.

---
+++ thesis/Samples/Stateful/SmokeDetectorTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class SmokeDetectorTest {
+
+	[Test]
+	public void OnAfterFiveThenOffAfterFivePlusTwenty () {
+		var detector = new SmokeDetector();
+
+		// 4 seconds at level of 0.6 accumulates detection
+		for (int i = 0; i < 4; i++) {
+			Assert.AreEqual (0, detector.Cycle (0.6));
+		}
+
+		// the 5th second at level 0.6 triggers alarm
+		Assert.AreEqual (1, detector.Cycle (0.6));
+
+		// alarm sounds while detection decays
+		// through 5 seconds at level 0.1
+		for (int i = 0; i < 5; i++) {
+			Assert.AreEqual (0, detector.Cycle (0.1));
+		}
+
+		// alarm sounds for another 20 seconds
+		for (int i = 0; i < 20; i++) {
+			Assert.AreEqual (0, detector.Cycle (0.1));
+		}
+
+		// 20 seconds without breaching level means
+		// it is safe to turn off alarm
+		Assert.AreEqual (-1, detector.Cycle (0.1));
+	}
+}
