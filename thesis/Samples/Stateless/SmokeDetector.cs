d7712fec63b08d17ef162f6129d0a35e516cd6eb
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-05-10 01:21:51 +0000

    Added the Stateless project, adapting the SmokeDetector and SmokeDetectorTest samples to trivially extract a stateless method, then write a test for a subset thereof.

---
+++ thesis/Samples/Stateless/SmokeDetector.cs
@@ -0,0 +1,42 @@
+﻿public class SmokeDetector {
+	public const double Level = 0.3;
+	public const int Danger = 5, WaitTime = 20;
+	private int time = 0, offTime = 0, detected = 0;
+	private bool alarmOn = false, waiting = false;
+
+	public int Cycle(double level) {
+		return Cycle(level, ref time, ref offTime, ref detected,
+			ref alarmOn, ref waiting);
+	}
+
+	internal static int Cycle(double level, ref int time,
+		ref int offTime, ref int detected,
+		ref bool alarmOn, ref bool waiting) {
+		int signal = 0;
+		time++;
+		if (level > Level && detected < Danger)
+			detected++;
+		else if (detected > 0)
+			detected--;
+		
+		if (!alarmOn && detected == Danger) {
+			alarmOn = true;
+			signal = 1;
+		}
+		
+		if (alarmOn) {
+			if (!waiting && detected == 0) {
+				waiting = true;
+				offTime = time + WaitTime;
+			}
+			if (waiting && detected == Danger)
+				waiting = false;
+			if (waiting && time > offTime) {
+				waiting = false;
+				alarmOn = false;
+				signal = -1;
+			}
+		}
+		return signal;
+	}
+}
