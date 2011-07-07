f223717f5618bd8af3d07f69fd2a23ef1ae62f48
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-07 18:05:29 +0000

    Introducing a variant on the Travelling Salesperson Problem that computes the great-circle distance between any two cities, assuming the salesperson will be flying.  Includes a unit test.

---
+++ thesis/Samples/Stateful/FlyingSalespersonProblemTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class FlyingSalespersonProblemTest
+{
+  /// <summary>
+  /// Based on the "Worked example" in Wikipedia's
+  /// article on "Great-circle distance",
+  /// confirmed with the "Great Circle Calculator" at
+  /// http://williams.best.vwh.net/gccalc.htm
+  /// </summary>
+  [Test]
+  public void ComputeTourLength_BnaToLax ()
+  {
+    // arrange
+    var cities = new double[][]
+    {
+      new double[] {1, 36.12, -86.67},
+      new double[] {2, 33.94, -118.40},
+    };
+    var fsp = new FlyingSalespersonProblem(cities);
+    fsp.VisitingOrder[0] = 1;
+    fsp.VisitingOrder[1] = 2;
+
+    // act
+    // since we computed to AND from, we divide by 2
+    var actual = fsp.ComputeTourLength() / 2;
+
+    // assert
+    Assert.AreEqual(2887.26, actual, 0.01);
+  }
+}
