84aaa447f3226beb3386cb2683f181a8ed54ca20
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-11 19:40:16 +0000

    Introducing a simplified and reflowed version of the CodeBlockModifier from the Textile project, with SME on the CodeFormatMatchEvaluator() method and two unit tests.

---
+++ thesis/Samples/Stateless/CodeBlockModifier.cs
@@ -0,0 +1,74 @@
+#region License Statement
+// Copyright (c) L.A.B.Soft.  All rights reserved.
+//
+// The use and distribution terms for this software are covered by the 
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by 
+// the terms of this license.
+//
+// You must not remove this notice, or any other, from this software.
+#endregion
+
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+#endregion
+
+class Globals
+{
+  public const string PunctuationPattern =
+    @"[\!""#\$%&'()\*\+,\-\./:;<=>\?@\[\\\]\^_`{}~]";
+}
+
+public class BlockModifier
+{
+  protected BlockModifier() { }
+  public virtual string ModifyLine(string line)
+  { return line; }
+  public virtual string Conclude(string line)
+  { return line; }
+}
+
+public class CodeBlockModifier : BlockModifier
+{
+  public override string ModifyLine(string line)
+  {
+    // Replace "@...@" zones with "<code>" tags.
+    line = Regex.Replace(line,
+      @"(?<before>^|([\s\([{]))" + // before
+       "@" +
+      @"(\|(?<lang>\w+)\|)?" +    // lang
+       "(?<code>[^@]+)" +        // code
+       "@" +
+      @"(?<after>$|([\]}])|(?=" +
+        Globals.PunctuationPattern +
+        @"{1,2}|\s|$))",  // after
+      CodeFormatMatchEvaluator);
+    return line;
+  }
+
+  static public string CodeFormatMatchEvaluator
+    (Match m)
+  {
+    return BuildCodeElementString(
+      m.Groups["before"].Value,
+      m.Groups["lang"].Value,
+      m.Groups["code"].Value,
+      m.Groups["after"].Value
+    );
+  }
+
+  internal static string BuildCodeElementString
+    (string before, string lang,
+    string code, string after)
+  {
+    string res = before + "<code";
+    if (lang.Length > 0)
+      res += " language=\"" + lang + "\"";
+    res += ">" + code + "</code>" + after;
+    return res;
+  }
+}
