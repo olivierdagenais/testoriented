2c9fd9077c6659c09fce74f45218f883feac8816
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-11 19:31:48 +0000

    Introducing a simplified and reflowed version of the CodeBlockModifier from the Textile project, with a unit test.

---
+++ thesis/Samples/Stateful/CodeBlockModifierTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class CodeBlockModifierTest
+{
+  [Test]
+  public void ModifyLine()
+  {
+    var cbm = new CodeBlockModifier();
+    const string input =
+      "Call the @|ruby|r_tohtml();@ method";
+    var actual = cbm.ModifyLine(input);
+    const string expected =
+      "Call the <code language=\"ruby\">r_tohtml();"
+      + "</code> method";
+    Assert.AreEqual(expected, actual);
+  }
+}
