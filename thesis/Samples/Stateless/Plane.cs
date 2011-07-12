66d9506665c5752b47bfa0bc9d7af3a25c27a65d
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-12 02:07:55 +0000

    For the Stateless version, extracted the DotProduct() method and added a unit test for it.

---
+++ thesis/Samples/Stateless/Plane.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Vector
+{
+  private readonly double _x, _y, _z;
+  public Vector(double x, double y, double z)
+  { _x = x; _y = y; _z = z; }
+  public double X { get { return _x; } }
+  public double Y { get { return _y; } }
+  public double Z { get { return _z; } }
+}
+
+public class Plane
+{
+  private readonly Vector _p1, _p2, _p3;
+  public Plane(Vector p1, Vector p2, Vector p3)
+  { _p1 = p1; _p2 = p2; _p3 = p3; }
+  public double DistanceFromPlane(Vector p)
+  {
+    var a = new Vector
+      (_p2.X - _p1.X, _p2.Y - _p1.Y, _p2.Z - _p1.Z);
+    var b = new Vector
+      (_p3.X - _p1.X, _p3.Y - _p1.Y, _p3.Z - _p1.Z);
+    var n = new Vector(
+      a.Y * b.Z - a.Z * b.Y,
+      a.Z * b.X - a.X * b.Z,
+      a.X * b.Y - a.Y * b.X
+    );
+    var length =
+      Math.Sqrt(n.X * n.X + n.Y * n.Y + n.Z * n.Z);
+    var normal =
+      new Vector(n.X/length, n.Y/length, n.Z/length);
+    var v = new Vector
+      (p.X - _p1.X, p.Y - _p1.Y, p.Z - _p1.Z);
+    var distance = DotProduct(v, normal);
+    return distance;
+  }
+  internal static double DotProduct
+    (Vector a, Vector b)
+  { return a.X * b.X + a.Y * b.Y + a.Z * b.Z; }
+}
