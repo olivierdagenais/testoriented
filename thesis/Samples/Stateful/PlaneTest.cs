97b9d09db881b34313ace0ad971c5e874c4cefec
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-12 01:17:49 +0000

    Introducing a Plane class (and its dependent Vector class) that illustrates some inline math and a unit test.

---
+++ thesis/Samples/Stateful/PlaneTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class PlaneTest
+{
+  [Test]
+  public void DistanceFromPlane_PointOnPlane()
+  {
+    var p1 = new Vector(0, 0, 0);
+    var p2 = new Vector(3, -3, 1);
+    var p3 = new Vector(4, 9, 2);
+    var p = new Plane(p1, p2, p3);
+    var actual = p.DistanceFromPlane(p3);
+    Assert.AreEqual(0, actual);
+  }
+}
