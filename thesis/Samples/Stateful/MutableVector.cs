a4be92f01b7b3954499bf8442dd0b9839ff8442c
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-13 01:37:25 +0000

    Introducing MutableVector and EuclideanVectorSpace to demonstrate a method that could still benefit from SME even though it does not manipulate HMS.

---
+++ thesis/Samples/Stateful/MutableVector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class MutableVector
+{
+  private double _x, _y, _z;
+  public MutableVector(double x, double y, double z)
+  { _x = x; _y = y; _z = z; }
+  public double X { get { return _x; } }
+  public double Y { get { return _y; } }
+  public double Z { get { return _z; } }
+  public void Normalize()
+  {
+    var length =
+      Math.Sqrt(_x * _x + _y * _y + _z * _z);
+    _x = _x / length;
+    _y = _y / length;
+    _z = _z / length; 
+  }
+}
+
+public class EuclideanVectorSpace
+{
+  public static double CalculateAngle
+    (MutableVector a, MutableVector b)
+  {
+    a.Normalize();  
+    b.Normalize();
+    var theta = Math.Acos(
+      a.X * b.X + a.Y * b.Y + a.Z * b.Z);
+    return theta * 180 / Math.PI;
+  }
+}
