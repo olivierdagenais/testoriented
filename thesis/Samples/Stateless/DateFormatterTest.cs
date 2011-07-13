745bafdd6dae612a3e472894c6758a4f33001e4f
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-13 15:14:14 +0000

    Introducing the proposed Stateless and Immutable attributes along with samples of their use, plus one unit test.

---
+++ thesis/Samples/Stateless/DateFormatterTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class DateFormatterTest
+{
+  [Test]
+  public void FormatIso8601Utc()
+  {
+    var actual = DateFormatter.
+      FormatIso8601Utc(2011, 07, 13, 14, 24, 38);
+    Assert.AreEqual("2011-07-13T14:24:38Z", actual);
+  }
+}
