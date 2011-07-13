11473101c0a40fb647573c94249135f3217ecb47
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-13 15:14:14 +0000

    Introducing the proposed Stateless and Immutable attributes along with samples of their use, plus one unit test.

---
+++ thesis/Samples/Stateless/ImmutableAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Class |
+                AttributeTargets.Struct,
+  AllowMultiple = false, Inherited = true)]
+public class ImmutableAttribute : Attribute
+{
+}
+
+[Immutable]
+public class EarthLocation
+{
+  private readonly double _lat, _lon, _alt;
+  public EarthLocation(double latitude,
+    double longitude, double altitude)
+  {
+    _lat = latitude; 
+    _lon = longitude;
+    _alt = altitude;
+  }
+  public double Latitude { get { return _lat; } }
+  public double Longitude { get { return _lon; } }
+  public double Altitude { get { return _alt; } }
+}
---
+++ thesis/Samples/Stateless/StatelessAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Method,
+  AllowMultiple = false, Inherited = true)]
+public class StatelessAttribute : Attribute
+{
+}
+
+public static class DateFormatter
+{
+  const string ExtendedIso8601Utc =
+    "{0:0000}-{1:00}-{2:00}T{3:00}:{4:00}:{5:00}Z";
+
+  [Stateless]
+  public static string FormatIso8601Utc(
+    int year, int month, int day,
+    int hour, int minute, int second)
+  {
+    var result = String.Format(
+      ExtendedIso8601Utc,
+      year, month, day, hour, minute, second);
+    return result;
+  }
+}
