a5f8e61464f02dbed0a68be41b944e394d27e9ee
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-14 20:56:16 +0000

    Introducing the stateless version of QuadraticEquation with the Root() method extracted.

---
+++ thesis/Samples/Stateless/QuadraticEquation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class QuadraticEquation
+{
+  private readonly double _a, _b, _c;
+  public QuadraticEquation
+    (double a, double b, double c)
+  { _a = a; _b = b; _c = c; }
+
+  public double[] ComputeRoots()
+  {
+    double[] result;
+    var discriminant = _b * _b - 4 * _a * _c;
+    if (discriminant == 0)
+    {
+      result = new[] { Root(_b, discriminant, _a) };
+    }
+    else if (discriminant < 0)
+    {
+      result = new double[0];
+    }
+    else
+    {
+      var sqrt = Math.Sqrt(discriminant);
+      result = new[]
+      {
+        Root(_b, sqrt, _a),
+        Root(_b, -sqrt, _a),
+      };
+    }
+    return result;
+  }
+
+  internal static double Root
+    (double b, double discriminant, double a)
+  {
+    return (-b + discriminant) / (2 * a);
+  }
+}
