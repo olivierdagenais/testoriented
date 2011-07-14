475084242aeaf783816fd90f132b4380b88caf70
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-14 19:57:00 +0000

    Introducing the PolarCoordinate, CartesianCoordinate and QuadraticEquation classes to use as examples.

---
+++ thesis/Samples/Stateful/QuadraticEquation.cs
@@ -0,0 +1,30 @@
+﻿public class QuadraticEquation
+{
+  private readonly double _a, _b, _c;
+  public QuadraticEquation
+    (double a, double b, double c)
+  { _a = a; _b = b; _c = c; }
+
+  public double[] ComputeRoots()
+  {
+    double[] result;
+    var discriminant = _b * _b - 4 * _a * _c;
+    if (discriminant == 0)
+    {
+      result = new[] { -_b / (2 * _a) };
+    }
+    else if (discriminant < 0)
+    {
+      result = new double[0];
+    }
+    else
+    {
+      result = new[]
+      {
+        (-_b + discriminant) / (2 * _a),
+        (-_b - discriminant) / (2 * _a),
+      };
+    }
+    return result;
+  }
+}
