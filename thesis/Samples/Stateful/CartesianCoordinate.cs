9411b5f0eac34b721c34b4df1d8c1d4e96274499
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-14 19:57:00 +0000

    Introducing the PolarCoordinate, CartesianCoordinate and QuadraticEquation classes to use as examples.

---
+++ thesis/Samples/Stateful/CartesianCoordinate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class PolarCoordinate
+{
+  private readonly double _r, _theta;
+  public PolarCoordinate(double r, double theta)
+  { _r = r; _theta = theta; }
+  public double R { get { return _r; } }
+  public double Theta { get { return _theta; } }
+}
+
+public class CartesianCoordinate
+{
+  private readonly double _x, _y;
+  public CartesianCoordinate(double x, double y)
+  { _x = x; _y = y; }
+
+  public PolarCoordinate ToPolar()
+  {
+    var xSquared = _x * _x;
+    var radians = Math.Atan2(_y, _x);
+    var ySquared = _y * _y;
+    var theta = radians * (180 / Math.PI);
+    var r = Math.Sqrt(xSquared + ySquared);
+    return new PolarCoordinate(r, theta);
+  }
+}
