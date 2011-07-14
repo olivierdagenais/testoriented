061af27d2890385edf6627ee409b72018178d1e4
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-14 17:42:48 +0000

    Introducing the Stateless version of SubversionClient, with two stateless methods extracted and two unit tests added.

---
+++ thesis/Samples/Stateless/SubversionClient.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Xml;
+
+public class Info {
+  public int Revision { get; set; }
+  public Uri Url { get; set; }
+  public Uri Root { get; set; }
+  public Guid Uuid { get; set; }
+}
+
+public partial class SubversionClient {
+  private readonly string _pathToSvnProgram;
+  public SubversionClient(string pathToSvnProgram)
+  { _pathToSvnProgram = pathToSvnProgram; }
+
+  internal static string CreateInfoArguments
+    (string pathToWorkingCopy) {
+    var sb = new StringBuilder();
+    sb.Append("info");
+    sb.Append(' ');
+    sb.Append('"');
+    sb.Append(pathToWorkingCopy);
+    sb.Append('"');
+    sb.Append(' ');
+    sb.Append("--xml");
+    return sb.ToString();
+  }
+
+  public Info Info(string pathToWorkingCopy) {
+    var arguments =
+      CreateInfoArguments(pathToWorkingCopy);
+
+    var output = ExecuteSvn(arguments);
+
+    var result = ParseInfoFromXml(output);
+    return result;
+  }
+
+  internal static Info ParseInfoFromXml
+    (string output) {
+    var doc = new XmlDocument();
+    doc.LoadXml(output);
+
+    var urlNode = doc.SelectSingleNode(
+      "/info/entry/url");
+    var rootNode = doc.SelectSingleNode(
+      "/info/entry/repository/root");
+    var uuidNode = doc.SelectSingleNode(
+      "/info/entry/repository/uuid");
+    var revisionNode = doc.SelectSingleNode(
+      "/info/entry/@revision");
+
+    var result = new Info {
+      Revision =
+        Convert.ToInt32(revisionNode.Value, 10),
+      Url = new Uri(urlNode.InnerText),
+      Root = new Uri(rootNode.InnerText),
+      Uuid = new Guid(uuidNode.InnerText),
+    };
+    return result;
+  }
+}
+
+public partial class SubversionClient {
+  private string ExecuteSvn(string arguments) {
+    string output;
+    using (var p = new Process()) {
+      p.StartInfo.UseShellExecute = false;
+      p.StartInfo.RedirectStandardOutput = true;
+      p.StartInfo.FileName = _pathToSvnProgram;
+      p.StartInfo.Arguments = arguments;
+      p.Start();
+      output = p.StandardOutput.ReadToEnd();
+      p.WaitForExit();
+    }
+    return output;
+  }
+}
