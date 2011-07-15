bf4c5dae1bacc467861707a2a6b8c007b0e5e894
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-15 13:38:55 +0000

    Introducing the "Stateless version" of MortonLayout where the DecodeAxis() method was extracted to show that sometimes, computations should stay together for readability and/or performance. Also noted same in subsection 3.3.2

---
+++ thesis/Samples/Stateless/MortonLayout.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Drawing;
+
+public class MortonLayout
+{
+  private const int NumberOfBits = 32;
+
+  public static Point Decode(int index)
+  {
+    var bits = new BitArray(new[] {index});
+    var x = DecodeAxis(bits, 1);
+    var y = DecodeAxis(bits, 0);
+    var result = new Point(x, y);
+    return result;
+  }
+
+  internal static int DecodeAxis
+    (BitArray bits, int offset)
+  {
+    var result = 0;
+    for (int o = 1, powerOfTwo = 1;
+        o < NumberOfBits; o += 2, powerOfTwo <<= 1)
+    {
+      if (bits[o - offset])
+      {
+        result += powerOfTwo;
+      }
+    }
+    return result;
+  }
+}
