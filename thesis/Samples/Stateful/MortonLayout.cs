e109435e6a597407431a338ca3789176f0f94e4c
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-07-15 12:49:28 +0000

    Importing (and adapting) the MortonLayout class from the PivotStack project to illustrate "computation coupling".

---
+++ thesis/Samples/Stateful/MortonLayout.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Drawing;
+
+public class MortonLayout
+{
+  private const int NumberOfBits = 32;
+
+  public static Point Decode(int index)
+  {
+    var bits = new BitArray(new[] {index});
+    int x = 0, y = 0;
+    for (int o = 1, powerOfTwo = 1; 
+        o < NumberOfBits; o += 2, powerOfTwo <<= 1)
+    {
+      if (bits[o - 1])
+      {
+        x += powerOfTwo;
+      }
+      if (bits[o])
+      {
+        y += powerOfTwo;
+      }
+    }
+    var result = new Point(x, y);
+    return result;
+  }
+}
