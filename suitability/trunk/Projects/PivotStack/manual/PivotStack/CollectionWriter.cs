a5c1e014036739b085e0a8017e42b8f8a48b20c6
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-08-28 14:42:07 +0000

    Make the PublicInterfaceComparer work with all projects. 1 - Upgraded to CORE 1.0.80 to get a CLI version of PublicInterfaceComparer since the NAnt version loads assemblies differently enough to fail with PivotStack. 2 - The use of the CLI version means we need to write to a file, then read back from it, hence using the lineCount task to do that. 3 - Introducing the manual strategy for the PivotStack project, which already has a few method extractions.

---
+++ suitability/trunk/Projects/PivotStack/manual/PivotStack/CollectionWriter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace PivotStack
+{
+    internal class CollectionWriter : InterceptingXmlWriter
+    {
+        private bool _isIntercepting;
+        private readonly Action<CollectionWriter> _callback;
+
+        public CollectionWriter (Stream output, XmlWriterSettings settings, Action<CollectionWriter> callback)
+            : base (output, settings)
+        {
+            _isIntercepting = false;
+            _callback = callback;
+        }
+
+        public override void WriteStartElement (string prefix, string localName, string ns)
+        {
+            base.WriteStartElement (prefix, localName, ns);
+            if (IsCollectionItems(localName, ns))
+            {
+                _isIntercepting = true;
+            }
+        }
+
+        internal static bool IsCollectionItems(string localName, string ns)
+        {
+            return Namespaces.Collection.NamespaceName == ns && "Items" == localName;
+        }
+
+        public override void WriteFullEndElement ()
+        {
+            if (_isIntercepting)
+            {
+                _callback (this);
+                _isIntercepting = false;
+            }
+            base.WriteFullEndElement ();
+        }
+    }
+}
---
+++ suitability/trunk/Projects/PivotStack/manual/PivotStack/Repositories/DatabaseRepositoryBase.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+
+using SoftwareNinjas.Core;
+
+namespace PivotStack.Repositories
+{
+    public abstract class DatabaseRepositoryBase
+    {
+        private readonly IDbConnection _connection;
+        public static readonly IDictionary<string, object> EmptyParameterDictionary =
+            new ReadOnlyDictionary<string, object> (new Dictionary<string, object> ());
+
+        protected DatabaseRepositoryBase(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return _connection;
+            }
+        }
+
+        internal IEnumerable<object[]> EnumerateRecords
+            (string commandText)
+        {
+            return EnumerateRecords (commandText, EmptyParameterDictionary);
+        }
+
+        internal IEnumerable<object[]> EnumerateRecords
+            (string commandText, IDictionary<string, object> parameters)
+        {
+            using (var command = _connection.CreateCommand ())
+            {
+                command.CommandText = commandText;
+                command.CommandTimeout = 0;
+                foreach (var pair in parameters)
+                {
+                    var param = command.CreateParameter ();
+                    param.ParameterName = pair.Key;
+                    param.Value = pair.Value;
+                    command.Parameters.Add (param);
+                }
+
+                using (var reader = command.ExecuteReader (CommandBehavior.SingleResult))
+                {
+                    Debug.Assert(reader != null);
+                    while (reader.Read ())
+                    {
+                        var destination = new object[reader.FieldCount];
+                        reader.GetValues (destination);
+                        yield return destination;
+                    }
+                }
+            }
+        }
+
+        internal static string LoadCommandText (string commandName)
+        {
+            using (var stream = AssemblyExtensions.OpenScopedResourceStream<DatabaseRepositoryBase> (commandName))
+            using (var reader = new StreamReader(stream))
+            {
+                var result = reader.ReadToEnd ();
+                return result;
+            }
+        }
+    }
+}
