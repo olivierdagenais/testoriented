8de6aec594bdfcf87d993abd88b2d960841a3cfa
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-09-03 20:58:11 +0000

    Create a manual strategy for the StringExtensions project, for consistency.

---
+++ suitability/trunk/Projects/StringExtensions/manual/StringExtensions/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace StringExtensions
+{
+    public class StringExtensions
+    {
+        public static string Capitalize(string value)
+        {
+            var sb = new StringBuilder();
+            bool word = false;  //  are  we  writing  a  word
+            foreach (var c in value)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (!word)
+                    {
+                        sb.Append(char.ToUpper(c));
+                        word = true;
+                    }
+                    else
+                        sb.Append(c);
+                }
+                else
+                {
+                    if (char.IsPunctuation(c))
+                        sb.Append('_');
+                    word = false;
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
