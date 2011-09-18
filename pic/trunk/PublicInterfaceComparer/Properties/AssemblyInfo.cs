1c73a1a82551a388ed85e91b03715f0cd187eece
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-09-18 15:20:32 +0000

    Adding base files.

---
+++ pic/trunk/PublicInterfaceComparer/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SoftwareNinjas.PublicInterfaceComparer")]
+[assembly: AssemblyDescription("Compares two assemblies for differences in their public members")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Software Ninjas")]
+[assembly: AssemblyProduct("Software Ninjas Public Interface Comparer")]
+[assembly: AssemblyCopyright("(C) Copyright 2011 Software Ninjas")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("552fd2ff-2b00-48d0-aa8d-cdcb0b554774")]
