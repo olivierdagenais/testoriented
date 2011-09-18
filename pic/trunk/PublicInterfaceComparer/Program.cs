5efd753e286d0f88057513bb754cdef762b6f800
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-09-18 15:20:32 +0000

    Adding base files.

---
+++ pic/trunk/PublicInterfaceComparer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using SoftwareNinjas.NAnt.Tasks;
+
+namespace SoftwareNinjas.PublicInterfaceComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pic = new PublicInterfaceComparerTask(false)
+            {
+                BaselineFile = new FileInfo(args[0]),
+                ChallengerFile = new FileInfo(args[1]),
+                ReportFile = new FileInfo(args[2]),
+            };
+            pic.ExecuteForTest();
+        }
+    }
+}
