b904a9bcb619155c1610bdd12b55298091aa73e5
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-09-18 15:20:52 +0000

    The PublicInterfaceComparer simply does not work as designed, so it is split off.

--- pic/trunk/PublicInterfaceComparer.Test/Properties/AssemblyInfo.cs
+++ pic/trunk/PublicInterfaceComparer.Test/Properties/AssemblyInfo.cs
@@ -5,14 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SoftwareNinjas.PublicInterfaceComparer")]
-[assembly: AssemblyDescription("Compares two assemblies for differences in their public members")]
+[assembly: AssemblyTitle("SoftwareNinjas.PublicInterfaceComparer.Test")]
+[assembly: AssemblyDescription("Tests for the public interface comparer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Software Ninjas")]
 [assembly: AssemblyProduct("Software Ninjas Public Interface Comparer")]
 [assembly: AssemblyCopyright("(C) Copyright 2011 Software Ninjas")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,5 +19,17 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("552fd2ff-2b00-48d0-aa8d-cdcb0b554774")]
-[assembly: InternalsVisibleTo("SoftwareNinjas.PublicInterfaceComparer.Test")]
+[assembly: Guid("85c6b762-04e5-4d68-99ef-f581af8ce09a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ pic/trunk/PublicInterfaceComparer/PublicInterfaceScanner.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using SoftwareNinjas.Core;
+
+namespace SoftwareNinjas.PublicInterfaceComparer
+{
+    /// <summary>
+    /// Scans the public interface of an <see cref="Assembly"/> instance.
+    /// </summary>
+    public class PublicInterfaceScanner
+    {
+        /// <summary>
+        /// The path to the baseline <see cref="Assembly"/>.
+        /// </summary>
+        public FileInfo BaselineFile
+        {
+            get;
+            set;
+        }
+
+        internal void Execute()
+        {
+            var baselineFullPath = BaselineFile.FullName;
+            var baseline = Assembly.LoadFile(baselineFullPath);
+        }
+
+        internal static IEnumerable<Type> GetVisibleTypes(Assembly assembly)
+        {
+            return assembly.GetTypes().Filter(t => t.IsVisible);
+        }
+
+        internal static IEnumerable<MemberInfo> GetVisibleMembers(Type type)
+        {
+            var unfiltered = type.GetMembers(BindingFlags.Public | BindingFlags.NonPublic
+                                             | BindingFlags.Instance | BindingFlags.Static);
+            var result = unfiltered.Filter(IsVisible);
+            return result;
+        }
+
+        internal static bool IsVisible(MemberInfo memberInfo)
+        {
+            if (memberInfo is MethodBase)
+            {
+                return IsVisible((MethodBase) memberInfo);
+            }
+            if (memberInfo is FieldInfo)
+            {
+                return IsVisible((FieldInfo) memberInfo);
+            }
+            if (memberInfo is EventInfo)
+            {
+                return true;
+            }
+            if (memberInfo is Type)
+            {
+                return IsVisible((Type) memberInfo);
+            }
+            if (memberInfo is PropertyInfo)
+            {
+                // we aren't processing them anyway; we instead focus on associated accessor methods
+                return false;
+            }
+            throw new ArgumentException("Parameter 'memberInfo' is an unsupported type.", "memberInfo");
+        }
+
+        internal static bool IsVisible(Type type)
+        {
+            return type.IsNested && (type.IsNestedPublic || type.IsNestedFamily || type.IsNestedFamORAssem);
+        }
+
+        internal static bool IsVisible(MethodBase methodBase)
+        {
+            return IsProtected(methodBase) || IsPublic(methodBase);
+        }
+
+        internal static bool IsProtected(MethodBase methodBase)
+        {
+            return methodBase.IsFamily || methodBase.IsFamilyOrAssembly;
+        }
+
+        internal static bool IsPublic(MethodBase methodBase)
+        {
+            return methodBase.IsPublic;
+        }
+
+        internal static bool IsVisible(FieldInfo fieldInfo)
+        {
+            return fieldInfo.IsPublic || fieldInfo.IsFamily || fieldInfo.IsFamilyOrAssembly;
+        }
+
+        internal static string Describe(MethodBase methodBase)
+        {
+            return "{0} {1}".FormatInvariant(Describe(methodBase.ReflectedType), methodBase.ToString());
+        }
+
+        internal static string Describe(Type type)
+        {
+            return type.FullName;
+        }
+
+        internal static string Describe(EventInfo eventInfo)
+        {
+            return "{0} {1}".FormatInvariant(Describe(eventInfo.ReflectedType), eventInfo.ToString());
+        }
+
+        internal static string Describe(FieldInfo fieldInfo)
+        {
+            return "{0} {1}".FormatInvariant(Describe(fieldInfo.ReflectedType), fieldInfo.ToString());
+        }
+
+        internal static string Describe(MemberInfo memberInfo)
+        {
+            if (memberInfo is MethodBase)
+            {
+                return Describe((MethodBase) memberInfo);
+            }
+            if (memberInfo is EventInfo)
+            {
+                return Describe((EventInfo) memberInfo);
+            }
+            if (memberInfo is FieldInfo)
+            {
+                return Describe((FieldInfo) memberInfo);
+            }
+            if (memberInfo is Type)
+            {
+                return Describe((Type) memberInfo);
+            }
+            throw new ArgumentException("Unsupported MemberInfo instance", "memberInfo");
+        }
+    }
+}
