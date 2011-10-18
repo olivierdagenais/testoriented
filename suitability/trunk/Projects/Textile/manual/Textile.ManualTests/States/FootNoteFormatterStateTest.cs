0384e5204c671454ff259e11938def1a727a77c5
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-10-18 22:12:50 +0000

    Updated Textile's manual tests to focus on the methods listed in table 4.1. The manual tests for the visibility and visibility-state strategies are now exploiting the visibility increases.

--- suitability/trunk/Projects/Textile/manual/Textile.ManualTests/States/FootNoteFormatterStateTest.cs
+++ suitability/trunk/Projects/Textile/manual/Textile.ManualTests/States/FootNoteFormatterStateTest.cs
@@ -9,6 +9,9 @@ namespace Textile.ManualTests.States
     [TestFixture]
     public class FootNoteFormatterStateTest
     {
+        /// <summary>
+        /// Tests the stateless part of the <see cref="FootNoteFormatterState.Enter()"/> method.
+        /// </summary>
         [Test]
         public void FormatFootNote()
         {
@@ -17,6 +20,19 @@ namespace Textile.ManualTests.States
 
             // assert
             Assert.AreEqual("<p id=\"fn1\" style=\"color:red;\"><sup>1</sup> ", actual);
+        }
+
+        /// <summary>
+        /// Tests the stateless part of the <see cref="FootNoteFormatterState.OnContextAcquired()"/> method.
+        /// </summary>
+        [Test]
+        public void ParseFootNoteId()
+        {
+            // act
+            var actual = FootNoteFormatterState.ParseFootNoteId("fn42");
+
+            // assert
+            Assert.AreEqual(42, actual);
         }
     }
 }
