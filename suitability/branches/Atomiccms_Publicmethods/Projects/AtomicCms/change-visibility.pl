3dd23b9c7ab78524b7a0059f94e7b7949e749e7a
nado18@gmail.com <nado18@gmail.com@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2011-10-19 23:26:07 +0000

    Adding script to change visibility of fields and properties from private/default to internal

---
+++ suitability/branches/Atomiccms_Publicmethods/Projects/AtomicCms/change-visibility.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+sub process()
+{
+	my $file = $File::Find::name;
+	return if $file !~ /\.cs$/i;
+
+	open(F, $file);
+	my @lines = <F>;
+	close(F);
+
+	open(F, ">$file");
+	foreach $_ (@lines)
+	{
+		/(^|[^\r\n])([\r\n]*)$/;
+		my $lf = $2;
+
+		s/(\r|\n)+//g;
+		visibilityState($_);
+		print F "$_$lf";
+	}
+	close(F);
+}
+
+sub visibilityState($)
+{
+	shift;
+	if( /^ {8}[^ {}#\/[][^(]+$/ )
+	{
+		if( ! /(public|internal)/ )
+		{
+			if( /private/ )
+			{
+				s/private/internal/;
+			}
+			else
+			{
+				s/^( {8})/\1internal /;
+			}
+		}
+	}
+	$_;
+}
+
+use File::Find;
+find(
+	{
+		wanted=>\&process,
+		follow=>1
+	},
+	"."
+);
