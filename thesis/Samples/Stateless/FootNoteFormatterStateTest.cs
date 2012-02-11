bb6763fa76ec7822a3ef2d2bac315f42f0af349f
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2012-02-11 18:52:47 +0000

    Copying the manually-written tests for FootNoteFormatterState from the Suitability project.

---
+++ thesis/Samples/Stateless/FootNoteFormatterStateTest.cs
@@ -0,0 +1,38 @@
+﻿using Textile.States;
+using NUnit.Framework;
+
+namespace Textile.ManualTests.States
+{
+    /// <summary>
+    /// A class to test <see cref="FootNoteFormatterState"/>.
+    /// </summary>
+    [TestFixture]
+    public class FootNoteFormatterStateTest
+    {
+        /// <summary>
+        /// Tests the stateless part of the <see cref="FootNoteFormatterState.Enter()"/> method.
+        /// </summary>
+        [Test]
+        public void FormatFootNote()
+        {
+            // act
+            var actual = FootNoteFormatterState.FormatFootNote(1, " style=\"color:red;\"");
+
+            // assert
+            Assert.AreEqual("<p id=\"fn1\" style=\"color:red;\"><sup>1</sup> ", actual);
+        }
+
+        /// <summary>
+        /// Tests the stateless part of the <see cref="FootNoteFormatterState.OnContextAcquired()"/> method.
+        /// </summary>
+        [Test]
+        public void ParseFootNoteId()
+        {
+            // act
+            var actual = FootNoteFormatterState.ParseFootNoteId("fn42");
+
+            // assert
+            Assert.AreEqual(42, actual);
+        }
+    }
+}
