a1e75680bc56149b4155503bee3f95d47ba1e0ae
olivier.dagenais <olivier.dagenais@47239c5b-3324-0410-9ae9-6b9b946bea8e>
2012-02-18 20:15:40 +0000

    Added an example of the PIC's output. Had to trim the spaces from the BlockModifier files before introducing 2 differences.

--- thesis/Samples/Stateful/BlockModifier.cs
+++ thesis/Samples/Stateful/BlockModifier.cs
@@ -1,23 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Textile
 {
-    public class BlockModifier
+  public class BlockModifier
+  {
+    protected internal BlockModifier()
     {
-        protected internal BlockModifier()
-        {
-        }
-
-        public virtual string ModifyLine(string line)
-        {
-            return line;
-        }
-
-        public virtual string Conclude(string line)
-        {
-            return line;
-        }
     }
+
+    public virtual string ModifyLine(string line)
+    {
+      return line;
+    }
+
+    public virtual string Conclude(string line)
+    {
+      return line;
+    }
+  }
 }
